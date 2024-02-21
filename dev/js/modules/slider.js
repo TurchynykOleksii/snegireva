91c055fe7feeea2ab79b508ed7f3ac8253c1bd4b
Turchynyk Oleksii <mishkalesha1994@gmail.com>
2024-02-21 19:49:16 +0200

    adaptive with desktop

--- dev/js/modules/slider.js
+++ dev/js/modules/slider.js
@@ -8,6 +8,16 @@ const swiperSertification = new Swiper(".certifications__slider", {
   },
   spaceBetween: 20,
   autoHeight: true,
+  breakpoints: {
+    615: {
+      loop: false,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      loop: false,
+    },
+  },
 });
 
 const swiper = new Swiper(".feetback__slider", {
