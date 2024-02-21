da44d4ae4127537dd66b773457a6c9ad15ea8e71
Turchynyk Oleksii <mishkalesha1994@gmail.com>
2024-02-21 07:58:54 +0200

    fix pc view

--- build/js/modules/slider.js
+++ build/js/modules/slider.js
@@ -1,39 +1,23 @@
-const swiper = new Swiper('.swiper', {
-	slidesPerView: "auto",
-	watchOverflow: true,
-	loop: true,
-	pagination: {
-		el: '.swiper-pagination',
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	spaceBetween: 20,
-	autoHeight: true,
-	breakpoints: {
-		// when window width is >= 992px
-		992: {
-			slidesPerView: 4,
-			spaceBetween: 40
-		}
-  }
+const swiperSertification = new Swiper(".certifications__slider", {
+  slidesPerView: 1,
+  watchOverflow: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  spaceBetween: 20,
+  autoHeight: true,
 });
 
-
-// $('.swiper-button-prev').remove();
-// $('.swiper-button-next').remove();
-
-// const swiper = new Swiper('.swiper', {
-// 	slidesPerView: 1,
-// 	watchOverflow: true,
-// 	pagination: {
-// 		el: '.swiper-pagination',
-// 	},
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	},
-// 	spaceBetween: 20,
-// 	autoHeight: true
-// });
+const swiper = new Swiper(".feetback__slider", {
+  slidesPerView: 1,
+  watchOverflow: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  spaceBetween: 20,
+  autoHeight: true,
+});
